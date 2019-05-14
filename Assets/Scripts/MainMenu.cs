2d5eac8287491db6c308831247ba26c1369b25ad
Jean Broqua <41450987+ElPastelina@users.noreply.github.com>
2019-05-14 14:27:14 -0300

    Mains menu and build
    - Added the main menu back
    - Created a playable build

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,18 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Start0()
-    {
-        SceneManager.LoadScene("Guri de carpete");
-    }
     public void Start1()
     {
         SceneManager.LoadScene("Bagual");
     }
-    public void Start2()
-    {
-        SceneManager.LoadScene("Peleia braba");
-    }
+
     public void Quit()
     {
         Application.Quit();
